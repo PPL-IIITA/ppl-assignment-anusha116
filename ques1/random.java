c1dfce783ae48ec6f43daae688938533ab1f12be
anusha116 <anusha.nimmala3572@gmail.com>
2017-02-26 10:06:42 -0800

    Add files via upload

---
+++ ques1/random.java
@@ -0,0 +1,10 @@
+package iit2015116;
+
+public class random {
+     public int getRandomNumberInRange(int min, int max) {
+		return (int)(Math.random() * ((max - min) + 1)) + min;
+	}
+    public float getRandomFNumberInRange(float min, float max) {
+		return (float)(Math.random() * ((max - min) + 1)) + min;
+	}
+}
